b0b32c559ca0f69c58a52a967c0074c28dcdcff6
unknown <psh4060144@gmail.com>
2025-02-10 00:54:53 +0900

    20250208 studyclub

--- 20250205/problem02.py
+++ 20250205/problem02.py
@@ -1,5 +1,3 @@
-############## 주의 ##############
-# 입력을 받기 위한 input 함수는 절대 사용하지 않습니다.
 # Python 제공 메서드 count 사용 시 감점
 
 def analyze_treasures(treasure_list, threshold):
@@ -23,15 +21,6 @@ def analyze_treasures(treasure_list, threshold):
 
     return (treasure_dict, threshold_sum)  # 만든 dict와 임계값을 초과하는 갯수를 반환.
 
-    # 여기에 코드를 작성하여 함수를 완성합니다.
-
-# 추가 테스트를 위한 코드 작성 가능
-# 예) print(함수명(인자))
-
-#####################################################
-# 아래 테스트 코드를 삭제하거나 수정하지 마세요.
-# 모든 책임은 삭제 혹은 수정한 본인에게 있습니다.
-############## 테스트 코드 삭제 금지 #################
 print(analyze_treasures(["gold", "silver", "gold", "diamond", "coin", "coin"], 1))
 # ({'gold': 2, 'silver': 1, 'diamond': 1, 'coin': 2}, 2)
 
@@ -40,4 +29,3 @@ print(analyze_treasures([], 3))
 
 print(analyze_treasures(["pearl", "pearl", "ruby"], 2))
 # ({'pearl': 2, 'ruby': 1}, 0)
-#####################################################
--- 20250205/problem08.py
+++ 20250205/problem08.py
@@ -1,43 +1,25 @@
-############## 주의 ##############
 # 입력을 받기 위한 input 함수는 절대 사용하지 않습니다.
-def point_sum(matrix, row_num, column_num):  # 상하좌우의 값을 더하는 함수.
-
-    if row_num -1 < 0:  # 범위를 벗어나는 값은 0으로 처리.
-        left = 0
-    else: left = matrix[row_num -1][column_num]
-
-    if row_num +1 > len(matrix):  # 범위를 벗어나는 값은 0으로 처리.
-        right = 0
-    else: right = matrix[row_num + 1][column_num]
-
-    if column_num -1 < 0:  # 범위를 벗어나는 값은 0으로 처리.
-        up = 0
-    else: up = matrix[row_num][column_num -1]
-
-    if column_num +1 > len(matrix):  # 범위를 벗어나는 값은 0으로 처리.
-        down = 0
-    else: down = matrix[row_num][column_num + 1]
-        
-    return left + right + up + down
 
 def max_adjacent_sum(matrix):
 
-    matrix_sum = 0
-    point_index = []
+    N = len(matrix)
+    around_row = [-1, 1, 0, 0, 0]
+    around_col = [0, 0, -1, 1, 0]
+    sum_max = 0
+    
+    for row in range(len(around_row)):
+        for col in range(len(around_row)):
+            sum = 0
+            
+            for i in range(5):
+                
+                if 0 <= row + around_row[i] < N and 0 <= col + around_col[i] < N:
+                    sum += matrix[row + around_row[i]][col + around_col[i]]
+                
+                if sum_max < sum:
+                    sum_max = sum
 
-    for row_num in range(len(matrix)):  # 행 갯수만큼 반복하여 행 위치를 먼저 설정
-        for column_num in range(len(matrix[row_num])):  # 열 갯수만큼 반복하여 열 위치를 설정.
-            all_sum = point_sum(matrix, row_num, column_num)  # 위에서 만들었던 상하좌우를 더하는 함수를 실행.
-            if all_sum > matrix_sum:
-                matrix_sum = all_sum  # 더 큰 수가 들어올때까지 반복하여여
-                point_index = [row_num, column_num]  # 해당 위치를 반환.
-
-    return point_index
-    # 여기에 코드를 작성하여 함수를 완성합니다.
-
-
-# 추가 테스트를 위한 코드 작성 가능
-# 예) print(함수명(인자))
+    return sum_max
 
 # 예시 행렬 데이터
 matrix1 = [
@@ -52,10 +34,5 @@ matrix2 = [
     [7, 8, 1]
 ]
 
-#####################################################
-# 아래 코드를 삭제하는 경우 
-# 모든 책임은 삭제한 본인에게 있습니다. 
-############## 테스트 코드 삭제 금지 #################
 print(max_adjacent_sum(matrix1))  # 29
 print(max_adjacent_sum(matrix2))  # 25
-#####################################################
